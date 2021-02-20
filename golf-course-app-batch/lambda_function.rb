a1f9845389d437541453220cc8420e2b8d30c798
vlsk3104 <vlsk3104@gmail.com>
2021-02-20 14:08:57 +0900

    エリアに所属する全てのゴルフ場を楽天 API で取得

--- golf-course-app-batch/lambda_function.rb
+++ golf-course-app-batch/lambda_function.rb
@@ -1,4 +1,4 @@
-
+require 'rakuten_web_service'
 
 module Area
   # 楽天APIで定められているエリアコード（8:茨城県,11:埼玉県,12:千葉県,13:東京都,14:神奈川県）
@@ -6,10 +6,24 @@ module Area
 end
 
 def lambda_handler(event:, context:)
+  RakutenWebService.configure do |c|
+    c.application_id = ENV['RAKUTEN_APPID']
+    c.affiliate_id = ENV['RAKUTEN_AFID']
+  end
+
   Area::CODES.each do |code| # 全てのエリアに対して以下操作を行う
-    # TODO: 1. このエリアのゴルフ場を楽天APIで全て取得する
+    1.upto(100) do |page|
+      #コース一覧を取得する（楽天APIの仕様上、一度に全てのゴルフ場を取得できないのでpageを分けて取得している）
+     courses = RakutenWebService::Gora::Course.search(areaCode: code, page: page)
+      courses.each do |course|
+        course_id - course['golfCourseId']
+        course_name = course['golfCourseName']
+        next if course_name.include?('レッスン')#ゴルフ場以外の情報（レッスン情報）をこれでスキップしてる
     # TODO: 2. 出発地点から取得したゴルフ場までの所要時間をGoogle Maps Platformで取得する
     # TODO: 3. 取得した取得した情報をDynamoDBに保存する
+      end
+      break unless courses.next_page? #次のページが存在するかどうか確認するメソッド
+   end
   end
 
   {statusCode: 200}
